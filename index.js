d98810a38756cc79e289c057ffe035f1b141b26c
stefian <stefan.ianta@w3ai.net>
2020-06-06 15:13:46 -0300

    added an events mem store var and push all events into it

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
   // Q: Should there be a dynamic list of Services / Registration for an Event bus?
   axios.post('http://localhost:4000/events', event);  // Posts Service
   axios.post('http://localhost:4001/events', event);  // Comments Service
