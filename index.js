cbcc16074ba9bdbdbffbd829121af56f8ccb20f1
stefian <stefan.ianta@w3ai.net>
2020-06-06 09:15:13 -0300

    added the Moderation Service to the list of services

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
   const event = req.body;
-
-  axios.post('http://localhost:4000/events', event);
-  axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+  // Q: Should there be a dynamic list of Services / Registration for an Event bus?
+  axios.post('http://localhost:4000/events', event);  // Posts Service
+  axios.post('http://localhost:4001/events', event);  // Comments Service
+  axios.post('http://localhost:4002/events', event);  // Query Service
+  axios.post('http://localhost:4003/events', event);  // Moderation Service
 
   res.send({ status: 'OK' });
 });
