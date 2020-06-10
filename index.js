f0b9849b057518d54102a5137e488c2b56863729
stefian <stefan.ianta@w3ai.net>
2020-06-10 16:09:38 -0300

    updated the routes with clusterip services

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.post('/events', (req, res) => {
   events.push(event);
   // Q: Should there be a dynamic list of Services / Registration for an Event bus?
   axios.post('http://posts-clusterip-srv:4000/events', event);  // Posts Service
-  // axios.post('http://localhost:4001/events', event);  // Comments Service
-  // axios.post('http://localhost:4002/events', event);  // Query Service
-  // axios.post('http://localhost:4003/events', event);  // Moderation Service
+  axios.post('http://comments-srv:4001/events', event);  // Comments Service
+  axios.post('http://query-srv:4002/events', event);  // Query Service
+  axios.post('http://moderation-srv:4003/events', event);  // Moderation Service
 
   res.send({ status: 'OK' });
 });
