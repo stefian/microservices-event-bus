9822d255d098869536a7533e47368c66a02e3e01
stefian <stefan.ianta@w3ai.net>
2020-06-10 09:00:24 -0300

    updated the localhost to posts-clusterip-srv for the posts route

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
   events.push(event);
   // Q: Should there be a dynamic list of Services / Registration for an Event bus?
-  axios.post('http://localhost:4000/events', event);  // Posts Service
-  axios.post('http://localhost:4001/events', event);  // Comments Service
-  axios.post('http://localhost:4002/events', event);  // Query Service
-  axios.post('http://localhost:4003/events', event);  // Moderation Service
+  axios.post('http://posts-clusterip-srv:4000/events', event);  // Posts Service
+  // axios.post('http://localhost:4001/events', event);  // Comments Service
+  // axios.post('http://localhost:4002/events', event);  // Query Service
+  // axios.post('http://localhost:4003/events', event);  // Moderation Service
 
   res.send({ status: 'OK' });
 });
